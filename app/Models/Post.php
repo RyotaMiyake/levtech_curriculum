8cc9109c9a094489482473d1df653e96d2836c15
RyotaMiyake <xxxryoutaxxx0702@gmail.com>
2022-09-17 03:44:28 +0900

    ブログ一覧表示

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    
+    public function getByLimit(int $limit_count = 10){
+        //updated_atで降順に並べた後，limitで件数制限をかける
+        return $this->orderBy('update_at', 'DESC')->limit($limit_count)->get();
+    }
+    
+    public function getPaginateByLimit(int $limit_count = 10){
+        return $this->orderBy('update_at', 'DESC')->paginate($limit_count);
+    }
 }
