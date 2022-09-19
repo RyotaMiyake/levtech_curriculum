80d415d5b55805655e576038008f41c5aca86e5d
RyotaMiyake <xxxryoutaxxx0702@gmail.com>
2022-09-20 02:59:58 +0900

    投稿作成画面追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\PostRequest;
 use App\Models\Post;
 
 class PostController extends Controller
@@ -22,5 +22,16 @@ class PostController extends Controller
 	public function show(Post $post){
 		return view('posts/show')->with(['post' => $post]);
 		//'post'はbladeファイルで使う変数。中身は$postはid=1のPostインスタンス。
+	}
+	
+	public function create(){
+		return view('posts/create');
+	}
+	
+	public function store(PostRequest $request, Post $post){
+		$input = $request['post'];
+		$post->timestamps = false;
+		$post->fill($input)->save();
+		return redirect('/posts/' . $post->id);
 	}
 }
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,9 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/posts', [PostController::class, 'index']);
-
+Route::get('/', [PostController::class, 'index']);
+Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 //'/posts/{対象データのID}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する
+Route::post('/posts', [PostController::class, 'store']);
 
-Route::get('/', [PostController::class, 'index']);
